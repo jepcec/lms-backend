bafdcfdf869a61fdffd3bb9173615e5d9f94a5b9
jepcec <jhonepc2003@gmail.com>
2026-04-29 15:44:21 -0500

    feature: connection prisma client with nestj

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './core/database/prisma.service';
 
 @Injectable()
 export class AppService {
+	constructor(private prisma: PrismaService){}
   getHello(): string {
     return 'Hello World!';
+  }
+  async getTestDb(): Promise<{}> {
+	try{
+		const result = await this.prisma.$queryRaw`
+			  SELECT table_name
+			  FROM information_schema.tables
+			  WHERE table_schema = 'public';
+			`;
+		return {status: "OK DB", message: result}
+	}
+	catch(error){
+		return {status: "NO DB", message_error: error}
+	}
   }
 }
