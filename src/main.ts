5530224df4ca284d2207fa4a12d268dd490527ff
jepcec <jhonepc2003@gmail.com>
2026-05-06 19:52:39 -0500

    update: tokens automatics in headers response

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule);
+	app.setGlobalPrefix('api')
+
+	// ================= cookies ===============
+	app.use(cookieParser())
+	app.enableCors({
+		origin: process.env.URL_FRONTEND,
+		credentials: true,
+	})
+	// =========================================
+
+	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
