72cfbbcd9419bcaeeee99cc3d8fdb8f5bbe943da
Ferdinan Condorcahua <ferdyj_CA@hotmail.com>
2026-05-11 20:52:46 -0500

    modificacion de schema y validacion de type en certificado

---
+++ src/modules/users/application/use-cases/set-enrollment-grade.use-case.ts
@@ -0,0 +1,59 @@
+// modules/users/application/use-cases/set-enrollment-grade.use-case.ts
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../../../../core/database/prisma.service';
+import { CertificateType } from '../../../../generated/prisma/client';
+
+@Injectable()
+export class SetGradeUseCase {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async execute(enrollmentId: string, score: number) {
+    // 1. Buscar la matrícula y validar existencia
+    const enrollment = await this.prisma.enrollment.findUnique({
+      where: { id: enrollmentId },
+    });
+
+    // Solución al error "possibly null"
+    if (!enrollment) {
+      throw new NotFoundException(`No se encontró la matrícula con ID: ${enrollmentId}`);
+    }
+
+    // 2. Validar que el curso esté completado al 100% [cite: 18, 19]
+    // Usamos .toNumber() porque progress_percent es Decimal en el schema
+    if (enrollment.progress_percent.toNumber() < 100) {
+      throw new BadRequestException('El estudiante aún no completa el 100% del curso');
+    }
+
+    // 3. Obtener la plantilla de certificado activa 
+    const activeTemplate = await this.prisma.certificateTemplate.findFirst({
+      where: { is_active: true }
+    });
+
+    if (!activeTemplate) {
+      throw new BadRequestException('No hay una plantilla de certificado activa en el sistema');
+    }
+
+    // 4. Determinar tipo de documento según la nota mínima (13)
+    const type = score >= 13 ? CertificateType.Certificado : CertificateType.Constancia;
+
+    // 5. Actualizar nota y emitir certificado/constancia
+    return await this.prisma.enrollment.update({
+      where: { id: enrollmentId },
+      data: {
+        final_score: score,
+        certificate: {
+          create: {
+            type: type,
+            template_id: activeTemplate.id,
+            pdf_url: 'URL_GENERADA_EN_S3_O_PROCESADA', // Aquí iría la lógica de tu servicio de PDF
+            // verification_code se genera automáticamente por el default(uuid) en el schema
+          }
+        }
+      },
+      include: { 
+        certificate: true,
+        course: true // Útil para tener el nombre del curso en la respuesta
+      }
+    });
+  }
+}
